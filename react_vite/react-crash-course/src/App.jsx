2d18e5e99f81a136857b118316384036892d117b
Gasijiyo <pys9307@naver.com>
2024-02-19 22:45:36 +0900

    .

--- react_vite/react-crash-course/src/App.jsx
+++ react_vite/react-crash-course/src/App.jsx
@@ -1,11 +1,17 @@
 import Post from "./components/Post";
 
 /**
- * import되는 컴포넌트는 대문자로 시작해야한다.
- * 소문자가 안되는 이유는 기본 HTML태그와의 구분 때문
+ * import되는 컴포넌트는 기본 HTML태그와의 구분 때문에 대문자로 시작해야한다.
  */
 function App() {
-  return <Post/>;
+  return (
+    <>
+      <Post/>
+      <Post/>
+      <Post/>
+    </>
+  );
+    
 }
 
 export default App;
