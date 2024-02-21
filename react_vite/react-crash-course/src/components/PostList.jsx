40b278a6b08e1434148245dd339b16afd57f19cc
Gasijiyo <pys9307@naver.com>
2024-02-21 22:13:52 +0900

    .

--- react_vite/react-crash-course/src/components/PostList.jsx
+++ react_vite/react-crash-course/src/components/PostList.jsx
@@ -1,8 +1,9 @@
 import Post from './Post';
+import classes from './PostList.module.css'
 
 function PostList() {
     return (
-        <ul>
+        <ul className={classes.posts}>
             <Post author="Maximlian" body="React.js is awesome"/>
             <Post author="Manuel" body="Check out the full course"/>
             <Post author="Yang Su" body="Handsome"/>
