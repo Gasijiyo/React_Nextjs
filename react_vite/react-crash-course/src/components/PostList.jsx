60b46417de32cdd33d2416f6960246ee0e9b10d6
Gasijiyo <pys9307@naver.com>
2024-03-07 20:32:08 +0900

    .

--- react_vite/react-crash-course/src/components/PostList.jsx
+++ react_vite/react-crash-course/src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
@@ -6,6 +6,18 @@ import Modal from "./Modal";
 
 function PostList({isPosing, onStopPosing}) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(()=>{
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8081/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
     fetch('http://localhost:8081/posts', {
@@ -26,17 +38,22 @@ function PostList({isPosing, onStopPosing}) {
           <NewPost onCancel={onStopPosing} onAddPost={addPostHandler}/>
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{textAlign: 'center', color: 'white'}}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
+      {isFetching && 
+        <div style={{textAlign: 'center', color: 'white'}}>
+          <p>Loading Posts...</p>
+        </div>
+      }
     </>
   );
 }
